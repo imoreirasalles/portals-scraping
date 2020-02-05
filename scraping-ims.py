160c9894d8beeb1eb3d4ba61003f5664d8b5648a
mechlerc <60672221+mechlerc@users.noreply.github.com>
2020-02-05 19:06:37 -0300

    primeira estrutura de pesquisa

--- scraping-ims.py
+++ scraping-ims.py
@@ -1,9 +1,26 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
 
-#open brownser and website
-browser = webdriver.Chrome() 
-browser.get('http://ims.com.br')
+#open webdriver and site
+with webdriver.Chrome() as driver:
+     driver.get('http://201.73.128.131:8080/portals/#/search')
+#find the search bar and writ
+     WebDriverWait(driver, 10).until(lambda s: s.find_element_by_id("search").is_displayed())
+     driver.find_element_by_id("search").send_keys("ferrez" + Keys.RETURN)
+     input()
+#close webdriver
+    driver.quit()
+     
+    
+    
 
-#close browser
-browser.quit()
+
+
+#with webdriver.Chrome() as driver:
+    #wait = WebDriverWait(driver, 10)
+    #driver.get("https://google.com.br/")
+    #driver.find_element_by_name("q").send_keys("cheese" + Keys.RETURN)
+    #first_result = wait.until(presence_of_element_located((By.CSS_SELECTOR, "h3>div")))
+    #print(first_result.get_attribute("textContent"))
