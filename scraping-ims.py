9422286bae3d97c3b1939632b3b565645fbd80d1
unknown <mechler.cintia@gmail.com>
2020-02-06 19:52:21 -0300

    Leitura de excel

--- scraping-ims.py
+++ scraping-ims.py
@@ -1,17 +1,25 @@
+import pandas as pd
+import xlrd
+from pandas import ExcelWriter
+from pandas import ExcelFile
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 
-#open webdriver and site
+#exc = pd.read_excel('teste.xlsx','Sheet1', index_col=None, na_values=['NA'])
 with webdriver.Chrome() as driver:
-     driver.get('http://201.73.128.131:8080/portals/#/search')
+    driver.get('http://201.73.128.131:8080/portals/#/search/')
+    WebDriverWait(driver, 10).until(lambda s: s.find_element_by_id("search").is_displayed())
+     driver.find_element_by_id("search").send_keys("001AAN014010" + Keys.RETURN)
+    url = driver.find_element_by_id('ng-href')
+    print (url)
+    input()
 #find the search bar and writ
-     WebDriverWait(driver, 10).until(lambda s: s.find_element_by_id("search").is_displayed())
-     driver.find_element_by_id("search").send_keys("ferrez" + Keys.RETURN)
-     input()
+     #WebDriverWait(driver, 10).until(lambda s: s.find_element_by_id("search").is_displayed())
+     #driver.find_element_by_id("search").send_keys("ferrez" + Keys.RETURN)
 #close webdriver
-    driver.quit()
+    #driver.quit()
      
     
     
